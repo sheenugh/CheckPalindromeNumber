cc17443e37aefbe1ea3f27dcfb39e97648bdcccf
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 00:15:18 +0800

    pseudo code

---
+++ check_if_palindrome_number.py
@@ -0,0 +1,8 @@
+
+# ----- FUNCTIONS -----
+
+# >>>>>>>>>> PSEUDO CODE <<<<<<<<<<
+# - This section is for the variable 'result' and printing the result
+# - Caller of the def function/s
+
+# ----- ACTUAL CODES -----
