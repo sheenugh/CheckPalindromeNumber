94eef87574ddde5c057abf2edfdca69af961b265
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-31 10:09:24 +0800

    may changes sa if statement. Instead to print outside the def, sa code block nalang ng if-else statement

--- check_if_palindrome_number.py
+++ check_if_palindrome_number.py
@@ -3,9 +3,11 @@
 def if_palindrome(string_num):
     checking = string_num[::-1]
     if string_num == checking:
-        return checking
+        print("The given number", string_num, "is a palindrome number.")
+        return 
     else:
-        return string_num
+        print("The given number", string_num, "is not a palindrome number.")
+        return 
 
 # >>>>>>>>>> PSEUDO CODE <<<<<<<<<<
 # ----- ACTUAL CODES -----
@@ -16,12 +18,11 @@ given_number2 = "125"
 # - This section is for the variable 'result' and printing a str, especially the result
 print("The original number is", given_number1 + ".")
 result1 = if_palindrome(given_number1)
-print("Thus, the given number " + str(result1) + " is a palindrome number.")
 
 print("\n")
 print("In addition, the original number is", given_number2 + ".")   
 result2 = if_palindrome(given_number2)
-print("Thus, the given number " + str(result2) + " is not a palindrome number.")
+
 
 
 
